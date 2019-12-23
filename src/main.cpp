ad621d3fbe7b9234921c7c6e42226a43c1f8ad72
enzo calogero <enzus@hotmail.com>
2019-12-23 23:34:52 +0000

    add lcd

--- src/main.cpp
+++ src/main.cpp
@@ -2,20 +2,52 @@
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
 #include "ThingSpeak.h"
-
-#define DHTPIN 4 // IMPORTANT D2 on NodeMCU is GPIO 4
+#define DHTPIN 3   //RX pin
+//#define DHTPIN 4 // IMPORTANT D2 on NodeMCU is GPIO 4
 
 #define DHTTYPE DHT11 // DHT 11
 
-const char *ssid = "xxxxxx"; // Your wifi ssid
-const char *password = "xxxxxxxxxxxx"; //Your wifi password
+const char *ssid = "xxxxxxxxx"; // Your wifi ssid
+const char *password = "xxxxxxxxxxxxx"; //Your wifi password
 
-unsigned long myChannelNumber = xxxxxxxxxxxxx;
-const char * myWriteAPIKey = "xxxxxxxxxxxxxxx";
+unsigned long myChannelNumber = xxxxxxxxx;
+const char * myWriteAPIKey = "xxxxxxxxxxxxx";
 
 const char* server = "api.thingspeak.com";
 
 WiFiClient client;
+
+
+// start
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
+
+
+
+#define BACKLIGHT_PIN     13
+
+// LiquidCrystal_I2C lcd(0x38);  // Set the LCD I2C address
+LiquidCrystal_I2C lcd(0x27,20,4);
+
+//LiquidCrystal_I2C lcd(0x38, BACKLIGHT_PIN, POSITIVE);  // Set the LCD I2C address
+
+
+// Creat a set of new characters
+const uint8_t charBitmap[][8] = {
+   { 0xc, 0x12, 0x12, 0xc, 0, 0, 0, 0 },
+   { 0x6, 0x9, 0x9, 0x6, 0, 0, 0, 0 },
+   { 0x0, 0x6, 0x9, 0x9, 0x6, 0, 0, 0x0 },
+   { 0x0, 0xc, 0x12, 0x12, 0xc, 0, 0, 0x0 },
+   { 0x0, 0x0, 0xc, 0x12, 0x12, 0xc, 0, 0x0 },
+   { 0x0, 0x0, 0x6, 0x9, 0x9, 0x6, 0, 0x0 },
+   { 0x0, 0x0, 0x0, 0x6, 0x9, 0x9, 0x6, 0x0 },
+   { 0x0, 0x0, 0x0, 0xc, 0x12, 0x12, 0xc, 0x0 }
+   
+};
+
+
+
+// end
 
 DHT dht(DHTPIN, DHTTYPE);
 
@@ -30,7 +62,9 @@ void updateThingSpeak(float t, float h){
 }
 
 void setup() {
-  // put your setup code here, to run once:
+  lcd.init();   // initializing the LCD
+  lcd.backlight();
+  
   Serial.begin(115200);
   dht.begin();
   ThingSpeak.begin(client);
@@ -39,15 +73,24 @@ void setup() {
     {
         delay(500);
         Serial.print("waiting for wifi to be connected");
+        lcd.setCursor(0, 0);
+        lcd.print("waiting for wifi to be connected");
     }
 
   Serial.println("");
   Serial.println("WiFi connected");
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("WiFi connected");
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
+
+
+ 
 }
 
 void loop() {
+    lcd.setBacklight(3);
     // put your main code here, to run repeatedly:
     delay(20000);
     float h = dht.readHumidity();
@@ -81,7 +124,21 @@ void loop() {
     Serial.print(" *C ");
     Serial.print(hif);
     Serial.println(" *F");
+
+
+   // lcd added
+  lcd.setCursor(0, 0);
+  String hs="Humidity: "+String((int)f)+" % ";
+  String ts="Temp: "+String((int)t)+" C ";
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print(ts);
+  lcd.setCursor(0, 1);
+  lcd.print(hs);
+ // lcd finished
+
     if(client.connect(server,80)){
         updateThingSpeak(t,h);
     }
+    
 }
